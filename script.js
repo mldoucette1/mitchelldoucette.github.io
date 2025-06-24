93aa581299802a055e38729f2ce70c3b73d9b3b8
mldoucette1 <97917533+mldoucette1@users.noreply.github.com>
2025-06-24 13:47:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const btn = document.getElementById("back-to-top");
+window.addEventListener("scroll", () => {
+  btn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+btn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
